c10814cd3a7bcdd338980a8f85d0fe33d98e253e
Manasi <54620499+m4nasi@users.noreply.github.com>
2021-02-26 20:12:31 +0000

    Update twitter.py

--- Twitter/twitter.py
+++ Twitter/twitter.py
@@ -17,8 +17,21 @@ message = "Hello test!"
 twitter.update_status(status=message)
 print("Tweeted: %s" % message)
 
-# image = open('image.jpg', 'rb')
-# response = twitter.upload_media(media=image)
-# media_id = [response['media_id']]
-# twitter.update_status(status=message, media_ids=media_id)
-# print("Tweeted: " + message)
+#messages = [
+    #"Hello World",
+    #"Hi there",
+    #"What's up?",
+    #"How's it going?",
+    #"Have you been here before?",
+    #"Get a hair cut!",
+#]
+
+#message = random.choice(messages)
+#twitter.update_status(status=message)
+#print("Tweeted: " + message)
+
+#image = open('image.jpg', 'rb')
+#response = twitter.upload_media(media=image)
+#media_id = [response['media_id']]
+#twitter.update_status(status=message, media_ids=media_id)
+#print("Tweeted: " + message)
